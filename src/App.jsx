c04c42ff0ee27b562a122c59e6cab726e0d2df13
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-05-15 23:12:31 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Console from './components/Console';
 import Home from './components/Home';
-import Document from './components/About';
 import ScrollToTop from "./components/ScrollToTop";
 import Login from './components/Login';
 import Signup from './components/Signup';
+import About from './components/About';
 
 function App() {
   return (
@@ -20,7 +20,7 @@ function App() {
           <Route path="/console" element={<Console />} >
             <Route path='filtered/:id' element={<Console />} />
           </Route>
-          <Route path='/about' element={<Document />} />
+          <Route path='/about' element={<About />} />
           <Route path='/login' element={<Login />} />
           <Route path='/signup' element={<Signup />} />
         </Routes>
